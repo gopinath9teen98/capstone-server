17455f4f6d6406c7501f7ed4d0a7aca1e1921877
Gopinath <gopinathaofficial@gmail.com>
2022-03-10 11:55:13 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+
+dotenv.config({path:"./config.env"})
+
+
+require("./db/conn");
+
+app.use(express.json());
+app.use(require("./router/auth"));
+
+
+
+const user = require("./model/dataSchema");
+
+app.listen(7000, () => {
+  console.log("running");
+});
